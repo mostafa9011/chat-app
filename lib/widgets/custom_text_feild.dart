57f65e702658ab09294f1040d8bff8a2f51313dc
mostafa <mostafarasekh5@gamil.com>
2024-02-16 09:55:59 -0800

    Finish Authentication for singUp,signIn

--- lib/widgets/custom_text_feild.dart
+++ lib/widgets/custom_text_feild.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFeild extends StatelessWidget {
-  const CustomTextFeild({super.key, required this.text});
+  const CustomTextFeild(
+      {super.key, required this.text, required this.onChanged});
   final String text;
+  final Function(String) onChanged;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (value) {
+        if (value == '') return 'field required !';
+      },
+      onChanged: onChanged,
       decoration: InputDecoration(
         enabledBorder: const OutlineInputBorder(
           borderSide: BorderSide(color: Colors.white),
